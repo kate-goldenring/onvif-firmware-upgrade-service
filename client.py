83890f5d59bdacb60887e505cd3238fd1b23b85a
Kate Goldenring <kate.goldenring@microsoft.com>
2021-11-01 13:33:33 -0700

    configurable ip address and sleep

--- client.py
+++ client.py
@@ -4,20 +4,25 @@ import onvif_firmware_update_pb2
 import onvif_firmware_update_pb2_grpc
 import os
 
+CAMERA_IP_ENV_VAR = "ONVIF_DEVICE_IP_ADDRESS"
+DESIRED_VERSION_ENV_VAR = "JOB_DESIRED_STATE"
+STATE_FILE_PATH = "AKRI_JOB_STATE_FILE_PATH"
+
 def run():
     print("-------------- Running --------------")
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
-    with grpc.insecure_channel('192.168.1.5:6052') as channel:
+    ip = os.getenv(CAMERA_IP_ENV_VAR, default = "localhost")
+    with grpc.insecure_channel(ip + ":6052") as channel:
         stub = onvif_firmware_update_pb2_grpc.FirmwareUpdateStub(channel)
         print("-------------- UpdateFirmware --------------")
-        version = os.environ['JOB_DESIRED_STATE']
+        version = os.environ[DESIRED_VERSION_ENV_VAR]
         print("Version requested is {}".format(version))
         request = onvif_firmware_update_pb2.UpdateFirmwareRequest(version=version, reboot_time_secs=30)
         response = stub.UpdateFirmware(request)
         print("Response is {}".format(response))
-        status_file = os.environ['AKRI_JOB_STATE_FILE_PATH']
+        status_file = os.environ[STATE_FILE_PATH]
         f = open(status_file, "w")
         f.write(response.version)
         f.close()
--- onvif_firmware_updater.py
+++ onvif_firmware_updater.py
@@ -1,27 +1,38 @@
 from concurrent import futures
 import logging
 import grpc
+import threading
+import time
 import onvif_firmware_update_pb2
 import onvif_firmware_update_pb2_grpc
 import os, subprocess, time
 
-START_CAMERA_SCRIPT_PATH = "/tmp/mock-onvif/debug-start-onvif-camera.sh"
+STOP_CAMERA_SCRIPT_PATH = "/home/kagold/firmware-poc/mock-onvif/stop-onvif-camera.sh"
+START_CAMERA_SCRIPT_PATH = "/home/kagold/firmware-poc/mock-onvif/debug-start-onvif-camera.sh"
+
 # FirmwareUpdater provides an implementation of the methods of the RouteGuide service.
 class FirmwareUpdateServicer(onvif_firmware_update_pb2_grpc.FirmwareUpdateServicer):
 
     def UpdateFirmware(self, request, context):
+        print("UpdateFirmware - called")
         requested_version = request.version
         timeout = request.reboot_time_secs
         print("UpdateFirmware - called with version {} and timeout {}".format(requested_version, timeout))
         if os.path.exists(START_CAMERA_SCRIPT_PATH):
-            print("exists")
-            # Needs to be sudo
-            # rc = subprocess.check_call([START_CAMERA_SCRIPT_PATH, "eno1"])
-            os.system("sudo {} {} {}".format(START_CAMERA_SCRIPT_PATH, "eno1", requested_version))
+            t1 = threading.Thread(target=update, args=(requested_version, int(timeout),))
+            t1.start()
         else: 
             print("ERROR script file DNE")
             # TODO: pull file
         return onvif_firmware_update_pb2.UpdateFirmwareReply(version = requested_version)
+
+def update(version, timeout):
+    os.system("sudo {}".format(STOP_CAMERA_SCRIPT_PATH))
+    print("Sleeping for {} seconds requested before restarting camera".format(timeout))
+    time.sleep(timeout)
+    # Needs to be sudo
+    # rc = subprocess.check_call([START_CAMERA_SCRIPT_PATH, "eno1"])
+    os.system("sudo {} {} {}".format(START_CAMERA_SCRIPT_PATH, "eno1", version))
 
 
 def serve():
